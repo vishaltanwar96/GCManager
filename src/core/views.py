79b87768b9507b5caef6d9f748a900bf7addae64
Vishal Tanwar <vishal.tanwar@outlook.com>
2021-03-20 17:08:00 +0530

    added AllGiftCardsTableView

--- src/core/views.py
+++ src/core/views.py
@@ -1,7 +1,19 @@
 from django.views.generic import TemplateView
+from django_tables2 import SingleTableView
+
+from core.tables import AllGiftCardsTable
+from core.models import GiftCardInformation
 
 
 class IndexView(TemplateView):
     """."""
 
     template_name = "core/index.html"
+
+
+class AllGiftCardsTableView(SingleTableView):
+    """."""
+
+    template_name = "core/allgcs.html"
+    table_class = AllGiftCardsTable
+    queryset = GiftCardInformation.objects.all()
