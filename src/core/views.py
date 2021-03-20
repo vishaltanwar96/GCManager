ea83eb56f5fdd32640f06962346f23b81ca1d21d
Vishal Tanwar <vishal.tanwar@outlook.com>
2021-03-20 20:29:53 +0530

    changed inheritance order

--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from django_tables2 import SingleTableView
+from django_tables2.export.views import ExportMixin
 
 from core.tables import AllGiftCardsTable
 from core.models import GiftCardInformation
@@ -11,7 +12,7 @@ class IndexView(TemplateView):
     template_name = "core/index.html"
 
 
-class AllGiftCardsTableView(SingleTableView):
+class AllGiftCardsTableView(ExportMixin, SingleTableView):
     """."""
 
     template_name = "core/allgcs.html"
