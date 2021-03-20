0d0275d1a4fde8f80b94d2f42494cf4bdea6149e
Vishal Tanwar <vishal.tanwar@outlook.com>
2021-03-20 17:07:45 +0530

    added url of all-gcs

--- src/core/urls.py
+++ src/core/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from core import views
 
-urlpatterns = [path("", views.IndexView.as_view(), name="index")]
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("all-gcs/", views.AllGiftCardsTableView.as_view(), name="all-gift-cards"),
+]
