ef79c28d776227c81f8b1cae217c104827cbcb60
Vishal Tanwar <vishal.tanwar@yougov.com>
2022-01-04 11:46:25 +0530

    reset project files

--- src/collector/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from collector import views
-
-urlpatterns = [
-    path("collectgc/", views.GiftCardCollectorView.as_view(), name="gc-collector")
-]
--- src/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from core import views
-
-urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("all-gcs/", views.AllGiftCardsTableView.as_view(), name="all-gift-cards"),
-]
