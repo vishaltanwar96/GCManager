68e9e210b50b54e05f93a4d14415eebc6c8340f9
Vishal Tanwar <vishal.tanwar@yougov.com>
2022-01-18 21:47:49 +0530

    use GiftCardFactory instead of manually creating the GiftCard instance

--- tests/unit/test_domain.py
+++ tests/unit/test_domain.py
@@ -2,31 +2,15 @@ import datetime
 from unittest import TestCase
 
 from gcmanager.domain import Denomination
-from gcmanager.domain import GiftCard
 from gcmanager.domain import GiftCardAssetSummary
-from gcmanager.domain import GiftCardID
 from gcmanager.domain import Money
-from gcmanager.domain import RedeemCode
+from tests.unit.factories import GiftCardFactory
 
 
 class TestGiftCard(TestCase):
     def test_returns_expected_date_of_expiry(self) -> None:
-        gift_card = GiftCard(
-            id=GiftCardID("blah-blah"),
-            redeem_code=RedeemCode("1234-XPGJ21-T3UF"),
+        gift_card = GiftCardFactory(
             date_of_issue=datetime.date(year=2022, month=1, day=3),
-            pin=10293910,
-            timestamp=datetime.datetime(
-                year=2022,
-                month=1,
-                day=4,
-                hour=13,
-                minute=10,
-                second=0,
-            ),
-            is_used=False,
-            denomination=Denomination(500),
-            source="AMAZON",
         )
         expected_expiry_date = datetime.date(year=2023, month=1, day=3)
         self.assertEqual(expected_expiry_date, gift_card.date_of_expiry)
