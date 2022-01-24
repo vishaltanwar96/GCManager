9c9011b0eec55958f030eb5bc050857e751e7d9f
Vishal Tanwar <vishal.tanwar@yougov.com>
2022-01-24 12:27:33 +0530

    Refactor on_get handler

--- gcmanager/webapi.py
+++ gcmanager/webapi.py
@@ -1,23 +1,17 @@
-import json
-
 import falcon
 
+from gcmanager.domain import SuccessfulResponse
+from gcmanager.serializers import GiftCardAssetSummarySerializer
 from gcmanager.usecases import GiftCardAssetInformationUseCase
 
 
 class GiftCardAssetInformationResource:
     def __init__(self, use_case: GiftCardAssetInformationUseCase) -> None:
         self._use_case = use_case
+        self._serializer = GiftCardAssetSummarySerializer()
 
     def on_get(self, request: falcon.Request, response: falcon.Response) -> None:
         assets = self._use_case.summarize()
-        response.text = json.dumps(
-            {
-                "status": "ok",
-                "data": {
-                    "total": assets.total,
-                    "used": assets.used,
-                    "unused": assets.unused,
-                },
-            },
-        )
+        serialized_data = self._serializer.dump(assets)
+        response.text = SuccessfulResponse(data=serialized_data).serialize()
+        response.status = falcon.HTTP_200
