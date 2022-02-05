b00f10de5473848a6a512d2c7d166d8bf596d079
Vishal <vishal.tanwar@outlook.com>
2022-02-05 22:56:52 +0530

    add custom TestCase MongoDBAndAppAwareTestCase

---
+++ tests/integration/db_app_test_case.py
@@ -0,0 +1,33 @@
+import os
+
+from falcon.testing import TestCase
+from pymongo import MongoClient
+
+from gcmanager.app import create_app
+
+
+class MongoDBAndAppAwareTestCase(TestCase):
+    """
+    A MongoDB and Falcon APP Aware Test Case
+    call super.setUp() before when using setup
+    to set things up in your own testcase.
+    """
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        os.environ["APP_ENV"] = "TEST"
+        cls.db_client = MongoClient("mongodb://testing_user:testing@localhost:27020/")
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.db_client.close()
+
+    def setUp(self) -> None:
+        super(MongoDBAndAppAwareTestCase, self).setUp()
+        self.app = create_app()
+        self.db = self.db_client["test-db"]
+        self.collection = self.db["test-collection"]
+
+    def tearDown(self) -> None:
+        self.collection.drop()
+        self.db_client.drop_database(self.db)
