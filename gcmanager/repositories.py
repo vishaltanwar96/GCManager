bb0d79897c3837449191e39963ec81299f781372
Vishal <vishal.tanwar@outlook.com>
2022-02-05 22:58:44 +0530

    add skeleton for GiftCardMongoDBRepository

--- gcmanager/repositories.py
+++ gcmanager/repositories.py
@@ -4,6 +4,8 @@ from abc import ABC
 from abc import abstractmethod
 from typing import Optional
 
+from pymongo.collection import Collection
+
 from gcmanager.domain import Denomination
 from gcmanager.domain import GiftCard
 from gcmanager.domain import GiftCardAssetSummary
@@ -58,4 +60,45 @@ class GiftCardRepository(ABC):
 
     @abstractmethod
     def get_summary(self) -> GiftCardAssetSummary:
+        pass
+
+
+class GiftCardMongoDBRepository(GiftCardRepository):
+    def __init__(self, gc_collection: Collection) -> None:
+        self._collection = gc_collection
+
+    def get_summary(self) -> GiftCardAssetSummary:
+        pass
+
+    def next_id(self) -> uuid.UUID:
+        pass
+
+    def timestamp(self) -> datetime.datetime:
+        pass
+
+    def get_available_denominations(self) -> list[Denomination]:
+        pass
+
+    def get_near_expiry_gift_card(
+        self,
+        denomination: Denomination,
+    ) -> Optional[GiftCard]:
+        pass
+
+    def update(self, gift_card_request: GiftCardUpdateRequest) -> None:
+        pass
+
+    def mark_used(self, gift_card_id: GiftCardID) -> None:
+        pass
+
+    def get_by_id(self, gift_card_id: GiftCardID) -> Optional[GiftCard]:
+        pass
+
+    def get_by_redeem_code(self, redeem_code: RedeemCode) -> Optional[GiftCard]:
+        pass
+
+    def create(self, gift_card: GiftCard) -> None:
+        pass
+
+    def get_unused(self) -> list[GiftCard]:
         pass
