571e7fee16122dd67278971cad90ead6d3782a99
Vishal <vishal.tanwar@outlook.com>
2022-02-05 22:55:54 +0530

    create settings related entities

---
+++ gcmanager/settings.py
@@ -0,0 +1,56 @@
+import os
+from abc import ABC
+from abc import abstractmethod
+from typing import NewType
+from typing import Type
+
+from gcmanager.enums import AppEnvironment
+
+Settings = NewType("Settings", dict)
+
+
+class AppSettings(ABC):
+    @staticmethod
+    @abstractmethod
+    def load() -> Settings:
+        pass
+
+
+class TestAppSettings(AppSettings):
+    @staticmethod
+    def load() -> Settings:
+        return Settings(
+            {
+                "MONGODB_USERNAME": "testing_user",
+                "MONGODB_PASSWORD": "testing",
+                "MONGODB_HOST": "localhost",
+                "MONGODB_PORT": "27020",
+                "MONGODB_DBNAME": "test-db",
+                "MONGODB_GC_COLLECTION_NAME": "test-collection",
+            },
+        )
+
+
+class ProdAppSettings(AppSettings):
+    @staticmethod
+    def load() -> Settings:
+        return Settings(
+            {
+                "MONGODB_USERNAME": os.environ.get("MONGODB_USERNAME"),
+                "MONGODB_PASSWORD": os.environ.get("MONGODB_PASSWORD"),
+                "MONGODB_HOST": os.environ.get("MONGODB_HOST"),
+                "MONGODB_PORT": os.environ.get("MONGODB_PORT"),
+                "MONGODB_DBNAME": os.environ.get("MONGODB_DBNAME"),
+                "MONGODB_GC_COLLECTION_NAME": os.environ.get(
+                    "MONGODB_GC_COLLECTION_NAME",
+                ),
+            },
+        )
+
+
+def settings_selector(enviroment: AppEnvironment) -> Type[AppSettings]:
+    env_settings_map = {
+        AppEnvironment.TEST: TestAppSettings,
+        AppEnvironment.PROD: ProdAppSettings,
+    }
+    return env_settings_map[enviroment]
