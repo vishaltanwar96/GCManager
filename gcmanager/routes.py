a67348c3da0e43083f4202c2aaed6587d78a9fcb
Vishal <vishal.tanwar@outlook.com>
2022-02-23 20:25:45 +0530

    create a dictionary mapping of route and view and register them all in a loop

--- gcmanager/routes.py
+++ gcmanager/routes.py
@@ -12,25 +12,18 @@ from gcmanager.webapi import NearExpiryGiftCardResource
 def make_router(container: Container) -> DefaultRouter:
     router = DefaultRouter()
     router.options.converters.update({"giftcardid": UUIDConverter})
-    router.add_route(
-        "/api/giftcards/assets/",
-        container[GiftCardAssetInformationResource],
-    )
-    router.add_route(
-        "/api/giftcards/{gift_card_id:giftcardid}/mark-used/",
-        container[MarkGiftCardUsedResource],
-    )
-    router.add_route(
-        "/api/giftcards/denominations/",
-        container[DenominationResource],
-    )
-    router.add_route(
-        "/api/giftcards/denominations/{denomination:int(min=10, max=10000)}/",
-        container[NearExpiryGiftCardResource],
-    )
-    router.add_route("/api/giftcards/", container[GiftCardResource])
-    router.add_route(
-        "/api/giftcards/{gift_card_id:giftcardid}/",
-        container[GiftCardResource],
-    )
+    route_template_view_mapping = {
+        "/api/giftcards/assets/": container[GiftCardAssetInformationResource],
+        "/api/giftcards/{gift_card_id:giftcardid}/mark-used/": container[
+            MarkGiftCardUsedResource
+        ],
+        "/api/giftcards/denominations/": container[DenominationResource],
+        "/api/giftcards/denominations/{denomination:int(min=10, max=10000)}/": (
+            container[NearExpiryGiftCardResource]
+        ),
+        "/api/giftcards/": container[GiftCardResource],
+        "/api/giftcards/{gift_card_id:giftcardid}/": container[GiftCardResource],
+    }
+    for route_template, view in route_template_view_mapping.items():
+        router.add_route(route_template, view)
     return router
