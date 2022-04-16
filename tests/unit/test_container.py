0ffc9f3bbe261754322f002fbe8105ba7b7db7cd
Vishal <vishal.tanwar@outlook.com>
2022-04-16 19:45:46 +0530

    fix tests

--- tests/unit/test_container.py
+++ tests/unit/test_container.py
@@ -14,9 +14,11 @@ class TestDependencyContainer(TestCase):
         when(os.environ).get("MONGODB_USERNAME").thenReturn(None)
         when(os.environ).get("MONGODB_PASSWORD").thenReturn(None)
         when(os.environ).get("MONGODB_HOST").thenReturn(None)
-        when(os.environ).get("MONGODB_PORT").thenReturn(None)
-        when(os.environ).get("MONGODB_DBNAME").thenReturn(None)
-        when(os.environ).get("MONGODB_GC_COLLECTION_NAME").thenReturn(None)
+        when(os.environ).get("MONGODB_PORT", "27017").thenReturn("27017")
+        when(os.environ).get("MONGODB_DBNAME", "gcmanager").thenReturn("gcmanager")
+        when(os.environ).get("MONGODB_GC_COLLECTION_NAME", "giftcards").thenReturn(
+            "giftcards",
+        )
         with self.assertRaises(ImproperlyConfigured):
             build_dependency_container()
 
@@ -25,7 +27,9 @@ class TestDependencyContainer(TestCase):
         when(os.environ).get("MONGODB_USERNAME").thenReturn("someuser")
         when(os.environ).get("MONGODB_PASSWORD").thenReturn("somepassword")
         when(os.environ).get("MONGODB_HOST").thenReturn("somehost")
-        when(os.environ).get("MONGODB_PORT").thenReturn(22222)
-        when(os.environ).get("MONGODB_DBNAME").thenReturn("somedb")
-        when(os.environ).get("MONGODB_GC_COLLECTION_NAME").thenReturn("somecollection")
+        when(os.environ).get("MONGODB_PORT", "27017").thenReturn("27017")
+        when(os.environ).get("MONGODB_DBNAME", "gcmanager").thenReturn("somedb")
+        when(os.environ).get("MONGODB_GC_COLLECTION_NAME", "giftcards").thenReturn(
+            "somecollection",
+        )
         self.assertIsInstance(build_dependency_container(), Container)
